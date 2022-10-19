02b435e4d62943e138f0a718fd8ea5c82f609b9b
nitesh <saininitesh452@gmail.com>
2022-10-19 21:55:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+let DUMMY_EXPENSE =[
+    
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+const[expenses,setExpenses]=useState(DUMMY_EXPENSE);
+//fetch api get request puted in a funtion
+function fetchData(){
+    fetch('http://192.168.88.195/poetryapis/expense.php').then(
+    response=>{
+        return response.json();
+    }
+).then(
+    data=>{
+        setExpenses(data);
+        }
+);
+
 }
 
+//here we use that func
+useEffect(()=>{
+    fetchData();
+},[]);
+
+
+const addExpenseHandler=(expense)=>{//new data comes in expense
+    
+    //we create a form for sending data in database 
+    const formData = new FormData();
+    formData.append('title',expense.title);
+    //'title' keyword must match with database column name
+    formData.append('amount',expense.amount);
+    //'amount' keyword must match with database column name
+    formData.append('date', expense.date);
+    //'date' keyword must match with database column name
+
+fetch('http://192.168.88.195/poetryapis/expensepost.php',{
+    method:'POST',
+    body:formData
+}).then(
+    response=>{
+        fetchData();
+    }
+);
+
+ 
+};
+
+    return (
+            <div>
+               <NewExpense onAddExpense={addExpenseHandler}/>
+               <Expenses item={expenses}/>
+               
+               
+            </div> 
+    );
+}
 export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,23 @@
+import './Expenses.css'
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props){
+    
+    return(
+        <div className='expenses'>
+{
+    props.item.map(
+        expense =>(
+            <ExpenseItem 
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date} />
+        )
+    )
+}
+        
+        </div>
+    );
+}
+export default Expenses;
